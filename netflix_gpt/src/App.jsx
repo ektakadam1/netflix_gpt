4eaf344e6a9b30f081cbc066d355f7cf723dab2f
ektakadam1 <ektakadam91@gmail.com>
2025-05-01 19:34:13 +0530

    Initial commit- codebase setup

---
+++ netflix_gpt/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 class=" flex justify-center text-3xl font-bold underline">
+      Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
